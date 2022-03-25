6d29e2814a818b99b896d4023361674468f64bb1
xzy <xzy__1997@163.com>
2022-03-25 16:07:24 +0800

    stable

--- src/main/java/com/boom/myblog/config/WebMvcConfigurer.java
+++ src/main/java/com/boom/myblog/config/WebMvcConfigurer.java
@@ -8,7 +8,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @Configuration
 public class WebMvcConfigurer extends WebMvcConfigurerAdapter {
 
-
     /**
      * 属性配置
      */
@@ -16,13 +15,9 @@ public class WebMvcConfigurer extends WebMvcConfigurerAdapter {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
 
-
-        //和页面有关的静态目录都放在项目的static目录下
-//        registry.addResourceHandler("/static/**").addResourceLocations("E:\\awesome-linchangzhi\\target\\");
-        //上传的图片在D盘下的OTA目录下，访问路径如：http://localhost:8081/OTA/d3cf0281-bb7f-40e0-ab77-406db95ccf2c.jpg
+        //上传的图片在D盘下的OTA目录下，访问路径如：http://localhost:9999/OTA/a.jpg
         //其中OTA表示访问的前缀。"file:D:/OTA/"是文件真实的存储路径
         registry.addResourceHandler("/static/**").addResourceLocations("file:"+pro.getStaticResource());
-//        System.out.println("file:" + pro.getAttachmentPath());
         registry.addResourceHandler("/OTA/**").addResourceLocations("file:" + pro.getOriginImagePath());
         registry.addResourceHandler("/OTA_th/**").addResourceLocations("file:" + pro.getThumbnailImagePath());
     }
--- src/main/java/com/boom/myblog/controller/SysUserController.java
+++ src/main/java/com/boom/myblog/controller/SysUserController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2022-03-25
  */
 @RestController
-@RequestMapping("/myblog/sys-user")
+@RequestMapping("/v1.0/sys-user")
 public class SysUserController {
 
 }
